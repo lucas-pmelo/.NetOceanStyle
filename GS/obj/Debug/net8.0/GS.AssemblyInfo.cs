eaa1236be1699f3d0eb139ab4f6c263f1e9a0653
lucas-pmelo <lucas.pinheiro.melo@gmail.com>
2024-06-03 21:27:03 -0300

    last touches

--- GS/obj/Debug/net8.0/GS.AssemblyInfo.cs
+++ GS/obj/Debug/net8.0/GS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ad9171b195d804de0f1f8c862b3fd85e6ab8683")]
 [assembly: System.Reflection.AssemblyProductAttribute("GS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
